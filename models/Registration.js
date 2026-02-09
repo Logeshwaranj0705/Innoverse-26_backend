f88d9ad8000f4bda31873d0050c6ddf01269ceab
logesh_07_05 <naru8sasu9344@gmail.com>
2026-02-09 22:20:43 +0530

    Update Registration.js

--- models/Registration.js
+++ models/Registration.js
@@ -1,20 +1,27 @@
 import mongoose from "mongoose";
 
-const memberSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  mobile: String
-});
+const memberSchema = new mongoose.Schema(
+  {
+    role: String,
+    name: String,
+    clg: String,
+    email: String,
+    mobile: String,
+    gender: String,
+    degree: String,
+    year: String,
+  },
+  { _id: false }
+);
 
 const registrationSchema = new mongoose.Schema({
-  teamName: String,
-  members: [memberSchema],
-  transactionId: String,
-  paymentImage: String,
-  submittedAt: {
-    type: Date,
-    default: Date.now
-  }
+  event: { type: String, default: "INNOVERSE 26" },
+  teamName: { type: String, required: true, unique: true },
+  teamSize: { type: Number, required: true },
+  members: { type: [memberSchema], required: true },
+  transactionId: { type: String, required: true },
+  paymentImage: { type: String, required: true },
+  submittedAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Registration", registrationSchema);
